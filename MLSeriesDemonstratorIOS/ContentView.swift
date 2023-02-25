57a6f179e48090f8f9cc7ecac3272409b70a1a5b
Ankur Kumar <ankur.kumar@outlook.in>
2023-02-25 09:43:28 +0530

    Initial Commit

---
+++ MLSeriesDemonstratorIOS/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MLSeriesDemonstratorIOS
+//
+//  Created by FCI-1275 on 25/02/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MLSeriesDemonstratorIOS/MLSeriesDemonstratorIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MLSeriesDemonstratorIOSApp.swift
+//  MLSeriesDemonstratorIOS
+//
+//  Created by FCI-1275 on 25/02/23.
+//
+
+import SwiftUI
+
+@main
+struct MLSeriesDemonstratorIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
